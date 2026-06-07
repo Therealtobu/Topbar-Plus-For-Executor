d7548e6350a1fd11bd9c49b2a9d26d765a1c5f1f
TobuTheXd <phamthien332013@gmail.com>
2026-06-07 08:49:22 +0700

    Update Attribute.lua

--- Attribute.lua
+++ Attribute.lua
@@ -2,7 +2,7 @@ return function(_M)
 task.defer(function()
 	local VERSION = _M.VERSION
 	local appVersion = VERSION.getAppVersion()
-	print("🍍 Running TopbarPlus " .. appVersion .. " (Executor Port) by @ForeverHD & HD Admin")
+	print("🌌 Running TopbarPlus " .. appVersion .. " (Executor Port) by @ForeverHD & Zephyr_Oganz")
 end)
 
 return {}
